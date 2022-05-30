2bf7a7ff00f9fa76cbd553ef6e589e3134196754
KongJin <pibrod@gmail.com>
2022-05-30 19:07:59 +0900

    Add: commentController 추가

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -4,29 +4,64 @@ import { client } from "../server";
 export const createComment = async (req: Request, res: Response) => {
   const userInfo = res.locals.userInfo;
   const { articleId, content } = req.body;
-  const comment = await client.comment.create({
-    data: {
-      user: {
-        connect: {
-          id: userInfo.id,
+  try {
+    const comment = await client.comment.create({
+      data: {
+        user: {
+          connect: {
+            id: userInfo.id,
+          },
         },
-      },
-      article: {
-        connect: {
-          id: articleId,
+        article: {
+          connect: {
+            id: articleId,
+          },
         },
+        content,
       },
-      content,
-    },
-  });
+    });
 
-  return res.json({ comment });
+    return res.json({ comment });
+  } catch {
+    return res.json({ comment: false });
+  }
 };
 
 export const updateComment = async (req: Request, res: Response) => {
-  return res.json("Update Comment!");
+  const userInfo = res.locals.userInfo;
+
+  const { commentId, content } = req.body;
+  try {
+    const comment = await client.comment.updateMany({
+      where: {
+        userId: userInfo.id,
+        id: commentId,
+      },
+      data: {
+        content,
+      },
+    });
+
+    return res.json({ comment });
+  } catch {
+    return res.json({ comment: false });
+  }
 };
 
 export const deleteComment = async (req: Request, res: Response) => {
-  return res.json("Delete Comment!");
+  const userInfo = res.locals.userInfo;
+
+  const { commentId } = req.body;
+  try {
+    const comment = await client.comment.deleteMany({
+      where: {
+        userId: userInfo.id,
+        id: commentId,
+      },
+    });
+
+    return res.json({ comment });
+  } catch {
+    return res.json({ comment: false });
+  }
 };
